75338d6706404f97a6d84b88d7cdb2b4a85756d6
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-15 22:40:17 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+// newHeader.id = 'victory'
+newHeader.setAttribute('id', 'victory')
+// newHeader.innerHTML = `<h1 id="victory">Ndoria is the champion</h1>`;
+newHeader.textContent = 'Ndoria is the champion'
